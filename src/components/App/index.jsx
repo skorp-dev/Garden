f0eb939cfcb4dd0eab6e7d6d39f9c97e59050a64
skorp-dev <skorpwebx@gmail.com>
2023-03-05 17:28:21 +0100

    component Footer

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "../Footer";
 import Nav from "../Nav";
 import "./index.css";
 
@@ -6,6 +7,8 @@ export default function App() {
   return (
     <div>
       <Nav />
+      <Footer/>
+      
     </div>
   );
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import instagramLogo from "../../icons/instagram.png";
+import whatsappLogo from "../../icons/whatsapp.png";
+import s from "./style.module.css";
+
+export default function Footer() {
+  return (
+    <div className={s.container}>
+      <div className={s.contacts}>
+        <div className={s.contact}>
+          <span>Contact</span>
+
+          <a href="tel:+4999999999">+49 999 999 99 99</a>
+
+          <div className={s.contact_link}>
+            <a href="/instgram">
+              <img src={instagramLogo} alt="instagram" />
+              <p>Instagram</p>
+            </a>
+
+            <a href="/whatsapp">
+              <img src={whatsappLogo} alt="whatsapp" />
+              <p>Whatsapp</p>
+            </a>
+          </div>
+        </div>
+
+        <div className={s.adress}>
+          <span>Adress</span>
+
+          <a href="http://telRan.de">
+            Linkstraße 2, 8 OG, 10785, Berlin, Deutschland
+          </a>
+
+          <div className={s.adress_hours}>
+            <span>Working Hours: </span>
+            <p>24 hours a day</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <iframe
+          className={s.iframe}
+          title="gogl"
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d19423.756635345188!2d13.3578823!3d52.5158896!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a8515353a68755%3A0xd0866511db4f838f!2sTel-Ran.de%20GmbH!5e0!3m2!1sru!2sde!4v1678022830586!5m2!1sru!2sde"
+        ></iframe>
+      </div>
+    </div>
+  );
+}
