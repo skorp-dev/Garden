d7569f13f45b5cf56ed96ac2903795e359aecdb2
skorp-dev <skorpwebx@gmail.com>
2023-03-06 19:46:59 +0100

    footer fix style

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -7,44 +7,39 @@ export default function Footer() {
   return (
     <div className={s.container}>
       <div className={s.contacts}>
-        <div className={s.contact}>
-          <span>Contact</span>
+        <span>Contact</span>
 
-          <a href="tel:+4999999999">+49 999 999 99 99</a>
+        <a className={s.contacts_tel} href="tel:+4999999999">
+          +49 999 999 99 99
+        </a>
 
-          <div className={s.contact_link}>
-            <a href="/instgram">
-              <img src={instagramLogo} alt="instagram" />
-              <p>Instagram</p>
-            </a>
-
-            <a href="/whatsapp">
-              <img src={whatsappLogo} alt="whatsapp" />
-              <p>Whatsapp</p>
-            </a>
-          </div>
-        </div>
-
-        <div className={s.adress}>
-          <span>Adress</span>
-
-          <a href="http://telRan.de">
-            Linkstraße 2, 8 OG, 10785, Berlin, Deutschland
+        <div className={s.contacts_links}>
+          <a href="/instgram">
+            <img src={instagramLogo} alt="instagram" />
+            <p>Instagram</p>
           </a>
 
-          <div className={s.adress_hours}>
-            <span>Working Hours: </span>
-            <p>24 hours a day</p>
-          </div>
+          <a href="/whatsapp">
+            <img src={whatsappLogo} alt="whatsapp" />
+            <p>Whatsapp</p>
+          </a>
         </div>
+
+        <span>Adress</span>
+
+        <a className={s.contacts_adress} href="http://telRan.de">
+          Linkstraße 2, 8 OG, 10785, Berlin, Deutschland
+        </a>
+
+        <p>Working Hours: </p>
+        <p className={s.adress_hours}>24 hours a day</p>
       </div>
-      <div>
-        <iframe
-          className={s.iframe}
-          title="gogl"
-          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d19423.756635345188!2d13.3578823!3d52.5158896!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a8515353a68755%3A0xd0866511db4f838f!2sTel-Ran.de%20GmbH!5e0!3m2!1sru!2sde!4v1678022830586!5m2!1sru!2sde"
-        ></iframe>
-      </div>
+
+      <iframe
+        className={s.iframe}
+        title="gogl"
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d19423.756635345188!2d13.3578823!3d52.5158896!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a8515353a68755%3A0xd0866511db4f838f!2sTel-Ran.de%20GmbH!5e0!3m2!1sen!2sde!4v1678022830586!5m2!1sen!2sde"
+      ></iframe>
     </div>
   );
 }
