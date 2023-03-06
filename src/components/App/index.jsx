f315f4e2628c226658eba863a237869dc761c2a2
skorp-dev <skorpwebx@gmail.com>
2023-03-06 20:48:18 +0100

    component SaleBanner

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "../Footer";
 import Nav from "../Nav";
+import Sale from "../Sale-banner";
 import "./index.css";
 
 export default function App() {
   return (
     <div>
       <Nav />
+      <Sale/>
       <Footer/>
       
     </div>
